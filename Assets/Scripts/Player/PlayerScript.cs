1267fadf02ae99c8ff1a95574012280c0d25a8d7
DoDaWu <102868333+noahwu8888@users.noreply.github.com>
2023-04-18 00:19:10 -0400

    Movement, Camera, and Dodge Roll are functional

--- Assets/Scripts/Player/PlayerScript.cs
+++ Assets/Scripts/Player/PlayerScript.cs
@@ -4,14 +4,53 @@ using UnityEngine;
 
 public class PlayerScript : MonoBehaviour
 {
-    public float speed = 5f;
+    #region General
+    [SerializeField] private float speed = 5f;
     private Rigidbody rb;
     private Vector2 moveInput;
+
+    private State state;
+    private enum State
+    {
+        Normal,
+        Rolling
+    }
+
+    #endregion
+
+    #region Roll
+    // speed multiplier while rolling
+    [SerializeField] private float rollMultiplier = 1.5f;
+    private Vector3 rollDirection;
+    // time player spends rolling
+    [SerializeField] private float rollLength = 3f;
+    private float rollTimer;
+    [SerializeField] private float rollCooldown = 1f;
+    
+    private float rollCooldownTimer;
+
+    void doRoll(Vector3 direction)
+    {
+        if (rollTimer > 0)
+        {
+            rb.velocity = direction * rollMultiplier;
+            rollTimer -= Time.deltaTime;
+        }
+        else
+        {
+            state = State.Normal;
+        }
+    }
+
+    #endregion
+
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        state = State.Normal;
+
     }
 
     // Update is called once per frame
@@ -19,16 +58,30 @@ public class PlayerScript : MonoBehaviour
     {
         moveInput.x = Input.GetAxisRaw("Horizontal"); // Get the raw input value for horizontal movement
         moveInput.y = Input.GetAxisRaw("Vertical");  // Get the raw input value for vertical movement
-
         moveInput.Normalize();
-        
-        rb.velocity = new Vector3(moveInput.x * speed, rb.velocity.y, moveInput.y * speed);
 
-        if(Input.GetButton("Fire3")){
+        if (rollCooldownTimer >= 0)
+        {
+            rollCooldownTimer -= Time.deltaTime;
+        }
 
+        Vector3 direction = new Vector3(moveInput.x * speed, rb.velocity.y, moveInput.y * speed);
+        if (Input.GetButton("Fire3") && rollCooldownTimer < 0)
+        {
+            rollTimer = rollLength;
+            state = State.Rolling;
+            rollDirection = direction;
+        }
+        switch (state)
+        {
+            case State.Normal:
+                rb.velocity = direction;
+                break;
+            case State.Rolling:
+                doRoll(rollDirection);
+                rollCooldownTimer = rollCooldown;
+                break;
         }
     }
-    void doRoll(){
-        
-    }
+
 }
